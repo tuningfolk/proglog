3752865598c5da5a0612650af3967d45a3869e59
tuningfolk <alisahadges@gmail.com>
2025-05-19 13:58:56 +0530

    added main.go

--- internal/server/main.go
+++ internal/server/main.go
@@ -1,3 +1,12 @@
 package main
 
-import( 
+import (
+	"log"
+
+	"github.com/tuningfolk/proglog/internal/server"
+)
+
+func main() {
+	srv := server.NewHTTPServer(":8080")
+	log.Fatal(srv.ListenAndServer())
+}
